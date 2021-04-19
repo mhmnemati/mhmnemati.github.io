0fac50c947886e234c36ce95821a7ec0f6814c06
KoLiBer <koliberr136a1@gmail.com>
2021-04-20 00:54:48 +0430

    feat: add arwes

---
+++ src/pages/index.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { ArwesThemeProvider, StylesBaseline, LoadingBars } from "@arwes/core";
+
+const FONT_FAMILY_ROOT = '"Titillium Web", sans-serif';
+const FONT_FAMILY_CODE = '"Source Code Pro", monospace';
+
+export default () => {
+    return (
+        <ArwesThemeProvider themeSettings={{}}>
+            <StylesBaseline
+                styles={{
+                    "html, body": { fontFamily: FONT_FAMILY_ROOT },
+                    "code, pre": { fontFamily: FONT_FAMILY_CODE },
+                }}
+            />
+
+            <LoadingBars
+                // animator={{ activate }}
+                size={2}
+                speed={4}
+                full
+            />
+        </ArwesThemeProvider>
+    );
+};
