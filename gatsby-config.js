652d1f7128a66e2defa751728a345f00564b2f78
KoLiBer <koliberr136a1@gmail.com>
2021-04-19 23:48:39 +0430

    chore: format project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,24 @@
 module.exports = {
-  siteMetadata: {
-    title: "KoLiBer personal website",
-  },
-  plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
+    siteMetadata: {
+        title: "KoLiBer personal website",
     },
-    "gatsby-plugin-mdx",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
-}
+    plugins: [
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon.png",
+            },
+        },
+        "gatsby-plugin-mdx",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "pages",
+                path: "./src/pages/",
+            },
+            __key: "pages",
+        },
+    ],
+};
