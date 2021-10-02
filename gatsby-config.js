a8794db0327986ba0c2e8b730840d7eac6fb4a12
KoLiBer <koliberr136a1@gmail.com>
2021-10-02 15:23:03 +0330

    chore: clean gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,17 +69,6 @@ module.exports = {
                     keySeparator: false,
                     nsSeparator: false,
                 },
-                pages: [
-                    // {
-                    //     matchPath: "/:lang?/blog/:uid",
-                    //     getLanguageFromPath: true,
-                    //     excludeLanguages: ["es"],
-                    // },
-                    // {
-                    //     matchPath: "/preview",
-                    //     languages: ["en"],
-                    // },
-                ],
             },
         },
         {
