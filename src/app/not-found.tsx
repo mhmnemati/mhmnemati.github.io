ddc72713e74aa59d30cf8a3f2ff3b6ffbc5072a2
KoLiBer <koliberr136a1@gmail.com>
2024-06-07 20:37:12 +0000

    fix: change not-found page style

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,20 +7,18 @@ export default function Page() {
     return (
         <>
             <Hero image="/images/hero.jpg">
-                <div className="flex flex-col gap-8">
+                <div className="flex flex-col items-center">
                     <Text as="h1">Page Not Found!</Text>
-                    <span>
-                        <Link href="/">
-                            <Frame
-                                as="button"
-                                size="small"
-                                type="hftagon"
-                                className="mt-8 px-8 py-4"
-                            >
-                                Goto Home
-                            </Frame>
-                        </Link>
-                    </span>
+                    <Link href="/">
+                        <Frame
+                            as="button"
+                            size="small"
+                            type="hftagon"
+                            className="mt-8 px-8 py-4"
+                        >
+                            Goto Home
+                        </Frame>
+                    </Link>
                 </div>
             </Hero>
         </>
