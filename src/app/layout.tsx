3cc427a94925c895bc813b1e4705bbcde7a46552
KoLiBer <koliberr136a1@gmail.com>
2024-06-07 17:04:41 +0000

    fix: refresh 404 problem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,25 @@
 import "./globals.css";
 import type { Metadata } from "next";
 
+import Headroom from "@/components/Headroom";
 import Theme from "@/components/Theme";
+import Frame from "@/components/Frame";
+import Modal from "@/components/Modal";
+import Text from "@/components/Text";
+import Link from "next/link";
 
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import {
+    faLinkedin,
+    faGithub,
+    faSkype,
+    faReact,
+} from "@fortawesome/free-brands-svg-icons";
 
 export const metadata: Metadata = {
     metadataBase: new URL("https://ckoliber.com"),
@@ -39,6 +53,119 @@ export const metadata: Metadata = {
     },
 };
 
+const links = [
+    ["Home", "/#home"],
+    ["About", "/#about"],
+    ["Resume", "/#resume"],
+    ["Experiences", "/experiences"],
+    ["Projects", "/projects"],
+    ["Library", "/library"],
+    ["Blog", "/blog"],
+];
+
+function Header() {
+    return (
+        <header className="w-full fixed z-10">
+            <Headroom>
+                <Frame
+                    size="small"
+                    type="underline"
+                    className="flex justify-start md:justify-end"
+                >
+                    <div className="block md:hidden">
+                        <Modal
+                            button={<FontAwesomeIcon icon={faBars} size="2x" />}
+                            className="flex flex-col gap-y-2"
+                        >
+                            {links.map(([title, href]) => (
+                                <Link key={title} href={href}>
+                                    <Frame
+                                        as="button"
+                                        size="small"
+                                        type="corners"
+                                        className="w-full p-4"
+                                    >
+                                        {title}
+                                    </Frame>
+                                </Link>
+                            ))}
+                        </Modal>
+                    </div>
+                    <div className="hidden md:block">
+                        {links.map(([title, href]) => (
+                            <Link key={title} href={href}>
+                                <Frame
+                                    as="button"
+                                    size="small"
+                                    type="hftagon"
+                                    className="m-2 px-4 py-2"
+                                >
+                                    {title}
+                                </Frame>
+                            </Link>
+                        ))}
+                    </div>
+                </Frame>
+            </Headroom>
+        </header>
+    );
+}
+
+function Footer() {
+    return (
+        <footer
+            className="flex flex-col justify-center text-center align-middle"
+            style={{
+                width: "100%",
+                height: "40vh",
+                backgroundImage: "url(/images/footer.jpg)",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+            }}
+        >
+            <span>
+                <Link
+                    className="text-inherit m-4"
+                    href={`mailto:ckoliber@gmail.com`}
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <FontAwesomeIcon icon={faEnvelope} size="2x" />
+                </Link>
+                <Link
+                    className="text-inherit m-4"
+                    href={`https://www.linkedin.com/in/ckoliber/`}
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <FontAwesomeIcon icon={faLinkedin} size="2x" />
+                </Link>
+                <Link
+                    className="text-inherit m-4"
+                    href={`https://github.com/ckoliber`}
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <FontAwesomeIcon icon={faGithub} size="2x" />
+                </Link>
+                <Link
+                    className="text-inherit m-4"
+                    href="https://join.skype.com/invite/fsPMhjWNv1rf"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <FontAwesomeIcon icon={faSkype} size="2x" />
+                </Link>
+            </span>
+            <br />
+            <Text manager="decipher" easing="outSine">
+                © Copyright 2021 KoLiBer • <FontAwesomeIcon icon={faReact} /> by
+                KoLiBer
+            </Text>
+        </footer>
+    );
+}
+
 export default function Layout(props: {
     children: React.ReactNode;
     header: React.ReactNode;
@@ -48,9 +175,9 @@ export default function Layout(props: {
         <html lang="en" className="scroll-smooth">
             <body dir="ltr">
                 <Theme>
-                    {props.header}
+                    <Header />
                     {props.children}
-                    {props.footer}
+                    <Footer />
                     <div id="modal" />
                 </Theme>
             </body>
